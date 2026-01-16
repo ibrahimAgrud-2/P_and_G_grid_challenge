717aaf597b7b7b2b1656ca78a545526c63d34ffb
ibrahim <ibrahimagrud7@gmail.com>
2026-01-16 22:26:04 +0300

    Complete UI design

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace P___G_Grid_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnHelp = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.btnClose = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnTest = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(2, 75);
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1041, 26);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.panel2.Controls.Add(this.btnHelp);
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(2, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1135, 47);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnHelp.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnHelp.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnHelp.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnHelp.FillColor = System.Drawing.Color.White;
+            this.btnHelp.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnHelp.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btnHelp.Location = new System.Drawing.Point(1021, 7);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnHelp.Size = new System.Drawing.Size(31, 32);
+            this.btnHelp.TabIndex = 13;
+            this.btnHelp.Text = "?";
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnClose.FillColor = System.Drawing.Color.White;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnClose.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btnClose.Location = new System.Drawing.Point(1086, 7);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnClose.Size = new System.Drawing.Size(31, 32);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "X";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(6, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Grid Challenge";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(1083, 75);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(54, 26);
+            this.lblTimer.TabIndex = 11;
+            this.lblTimer.Text = "0:40";
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnTest.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnTest.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnTest.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnTest.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnTest.FillColor = System.Drawing.SystemColors.WindowText;
+            this.btnTest.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnTest.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btnTest.Location = new System.Drawing.Point(553, 594);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnTest.Size = new System.Drawing.Size(203, 32);
+            this.btnTest.TabIndex = 14;
+            this.btnTest.Text = "test button";
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.BackColor = System.Drawing.Color.Wheat;
+            this.txtX.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtX.Location = new System.Drawing.Point(182, 582);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(147, 32);
+            this.txtX.TabIndex = 15;
+            this.txtX.Text = "10";
+            // 
+            // txtY
+            // 
+            this.txtY.BackColor = System.Drawing.Color.Wheat;
+            this.txtY.Location = new System.Drawing.Point(182, 630);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(147, 32);
+            this.txtY.TabIndex = 16;
+            this.txtY.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 585);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 26);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "test X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 636);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 26);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "test Y";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1148, 423);
+            this.panel1.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Items.AddRange(new object[] {
+            "LOcasyonlaerı dizilere ekleyerek üst üste gelmesini engellemeye çalıştım. Ancak b" +
+                "ir sorun ",
+            "var, normalde de zaten tam üst üste gelmiyorlar ki. Benim bu arrayli çözümüm sade" +
+                "ce ",
+            "tam olarak üst üste geleyi engelleri ki oda çok düşük ihtimak.",
+            "",
+            "",
+            "En son şöyle bir şey düşündüm:",
+            "Yeşil kalemle çizdiğim panel\'de olduğu gibi her X-Y kesişimi bir ",
+            "aday buton noktası olabilir. program butonları buradan bir yere yerleştirebilir"});
+            this.listBox1.Location = new System.Drawing.Point(754, 253);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 721);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTimer;
+        private Guna.UI2.WinForms.Guna2CircleButton btnHelp;
+        private Guna.UI2.WinForms.Guna2CircleButton btnClose;
+        private Guna.UI2.WinForms.Guna2CircleButton btnTest;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace P___G_Grid_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ibrahimAgrud-2");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        //Generate Random number
+        //______________________________
+        private static Random random;
+        private static void Init()
+        {
+            if (random == null) random = new Random();
+        }
+        public static int Random(int min, int max)
+        {
+            Init();
+            return random.Next(min, max);
+        }
+        public static int GetInt32(int minValue, int maxValue)
+        {
+            if (minValue >= maxValue)
+                throw new ArgumentOutOfRangeException();
+
+            int diff = maxValue - minValue;
+            byte[] uint32Buffer = new byte[4];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                while (true)
+                {
+                    rng.GetBytes(uint32Buffer);
+                    uint rand = BitConverter.ToUInt32(uint32Buffer, 0);
+
+                    long max = (1L + uint.MaxValue) / diff * diff;
+
+                    if (rand < max)
+                        return (int)(minValue + (rand % diff));
+                }
+            }
+        }
+        //______________________________
+
+        int[] Xs = new int[50];
+        int[] Ys = new int[50];
+
+ 
+        Guna2CustomRadioButton createRadioButton()
+        {
+            Guna2CustomRadioButton rb = new Guna2CustomRadioButton();
+
+
+            rb.CheckedState.FillColor = System.Drawing.Color.White;
+            rb.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(64, 0, 0);
+            rb.AutoSize = false;
+            rb.Size = new System.Drawing.Size(27, 27);
+
+            //_____________________________
+
+        //    rb.Location = new Point(11, 111);
+   
+            return rb;
+        }
+
+
+        void testForAddRadioButtonsInLoop()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                panel1.Controls.Add(createRadioButton());
+
+            }
+        }
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            testForAddRadioButtonsInLoop();
+        }
+
+    
+    }
+}
